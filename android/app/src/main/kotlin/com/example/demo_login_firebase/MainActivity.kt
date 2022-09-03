dbd9530050de8cca125efe3a6fc2c0b5abd2b5c5
ducancon1990 <107626157+ducancon1990@users.noreply.github.com>
2022-09-03 22:30:53 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/demo_login_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_login_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+class ProfilePage extends StatefulWidget {
+  final User user;
+
+  const ProfilePage({super.key, required this.user});
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool _isSigningOut = false;
+
+  late User _currentUser;
+
+  @override
+  void initState() {
+    _currentUser = widget.user;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'NAME: ${_currentUser.displayName}',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              'EMAIL: ${_currentUser.email}',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(height: 16.0),
+            _isSigningOut
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        _isSigningOut = true;
+                      });
+                      await FirebaseAuth.instance.signOut();
+                      setState(() {
+                        _isSigningOut = false;
+                      });
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => LoginPage(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: const Text('Sign out'),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
